09910ae1c15fe8d7beb487aa11f73d9e9499f780
Desktop <mart.simon17@gmail.com>
2022-12-17 17:58:58 +0100

    test

--- 9.ŠimonM.SemestralProject/src/main.java
+++ 9.ŠimonM.SemestralProject/src/main.java
@@ -19,7 +19,6 @@ public class main {
                     double[][] vypis = tools.SortSouradnic(souradnice, primka);
                     // vypis souradnic (promena "vypis")
                     tools.vypisSouradnic(vypis);
-                }
                 case "n", "N" -> {
                     System.out.println("Ukončuji program");
                     endProgram = true;
